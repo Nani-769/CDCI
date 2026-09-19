c611b65dd27a45c5cf9ff03daea4e83b91a1edb0
Nani-769 <rellaramu1818@gmail.com>
2026-09-19 21:06:46 +0530

    delete compoentn

---
+++ src/app/chemo-visit/chemo-visit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chemo-visit',
+  templateUrl: './chemo-visit.component.html',
+  styleUrls: ['./chemo-visit.component.css']
+})
+export class ChemoVisitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
